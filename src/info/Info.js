580593958d5bb8656053993a954fa01276af6ca9
Tejas Vaity <tejas.vaity3@gmail.com>
2025-11-10 01:04:15 -0500

    added a new project of aws chatbot

--- src/info/Info.js
+++ src/info/Info.js
@@ -3,6 +3,7 @@ import project_1 from "../img/project_1.png"
 import project_2 from "../img/project_2.png"
 import project_3 from "../img/project_3.png"
 import project_4 from "../img/project_4.png"
+import project_5 from "../img/project_5.png"
 
 export let colors = ["rgb(10, 255, 75)", "rgb(68, 0, 255)"];
 
@@ -97,15 +98,24 @@ export const info = {
         },
         {
             title: "Project 3",
-            live: "https://textify-kappa-liard.vercel.app/",
-            source: "https://github.com/Tej619/Textify.git",
+            live: "https://euro-chatbot-v2.s3.us-east-1.amazonaws.com/index.html",
+            source: "https://github.com/Tej619/ECE-528-Cloud-Computing.git",
             image: project_3
         },
         {
             title: "Project 4",
-            live: "https://github.com/user-attachments/assets/c1f7359b-16db-40dd-8df7-ce9f8ed5b836",
-            source: "https://github.com/Tej619/Text_to_Speech.git",
+            live: "https://textify-kappa-liard.vercel.app/",
+            source: "https://github.com/Tej619/Textify.git",
             image: project_4
         },
+        {
+            title: "Project 5",
+            live: "https://github.com/user-attachments/assets/c1f7359b-16db-40dd-8df7-ce9f8ed5b836",
+            source: "https://github.com/Tej619/Text_to_Speech.git",
+            image: project_5
+        },
+        {
+
+        }
     ],
 }
